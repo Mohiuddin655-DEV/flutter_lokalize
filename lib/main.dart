d59f7d11c3990afbd9574c3bfeaa41ebccc48b1b
Md. Mohi-Uddin <mohiuddin655.bd@gmail.com>
2024-12-11 13:18:52 +0600

    init

--- lib/main.dart
+++ lib/main.dart
@@ -6,8 +6,8 @@ import 'app/app.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Lokalise.init(
-    projectId: '69406755673aeba009d1a0.72786162',
-    sdkToken: '266eb19742394e8f2da2c4b6d02a4ad44c891688',
+    projectId: 'FILL YOUR PROJECT ID',
+    sdkToken: 'FILL YOUR SDK TOKEN',
   );
   runApp(const App());
 }
